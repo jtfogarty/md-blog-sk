f18050e23f2b6d1a051e2f1558e42eccaccc5be2
jtfogarty <jeff@fogarty.org>
2022-12-31 14:36:50 -0600

    added svelte-splitpanes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import splitpanes from 'svelte-splitpanes';
+
+let Splitpanes = splitpanes;
+</script>
+
+{#if Splitpanes}
+	<svelte:self this={Splitpanes} />
+{/if}
+
+<Splitpanes>
+	<div>Pane 1</div>
+	<div><slot /></div>
+</Splitpanes>
